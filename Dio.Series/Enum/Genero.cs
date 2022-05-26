ccfcf09e098d5965c8e06a4eb5f6c044cbfb2c77
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2022-05-26 20:09:30 -0300

    Arquivos base do projeto

---
+++ Dio.Series/Enum/Genero.cs
@@ -0,0 +1,16 @@
+namespace Dio.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Fantasia = 6,
+        Ficcao_Cientifica = 7,
+        Romance = 8,
+        Suspensa = 9,
+        Terror = 10
+    }
+}
---
+++ Dio.Series/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
